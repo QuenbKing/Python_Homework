238567d92b2b1d1eb493be7da9c9adb830d9eb8f
QuenbKing <Nikita.Karskanov@urfu.me>
2023-11-19 03:19:12 +0500

    Make fields private in class LinkedList

--- Module13/Task4/MyLinkedList.py
+++ Module13/Task4/MyLinkedList.py
@@ -15,54 +15,54 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        self.count = 0
-        self.head = None
-        self.tail = None
-        self.current_iterator = self.head
+        self.__count = 0
+        self.__head = None
+        self.__tail = None
+        self.__current_iterator = self.__head
 
     def append(self, value):
         node = Node(value)
-        if self.count == 0:
-            self.head = self.tail = node
+        if self.__count == 0:
+            self.__head = self.__tail = node
         else:
-            self.tail.set_next(node)
-            self.tail = node
-        self.count += 1
+            self.__tail.set_next(node)
+            self.__tail = node
+        self.__count += 1
 
     def get(self, index):
-        if index < 0 or index >= self.count:
+        if index < 0 or index >= self.__count:
             raise IndexError
 
-        cur_node = self.head
+        cur_node = self.__head
         for _ in range(index):
             cur_node = cur_node.get_next()
         return cur_node.get_data()
 
     def remove(self, index):
-        if index < 0 or index >= self.count:
+        if index < 0 or index >= self.__count:
             raise IndexError
 
         if index == 0:
-            self.head = self.head.get_next()
+            self.__head = self.__head.get_next()
         else:
-            current = self.head
+            current = self.__head
             for _ in range(index - 1):
                 current = current.get_next()
             current.set_next(current.get_next().get_next())
-            if index == self.count - 1:
-                self.tail = current
-        self.count -= 1
+            if index == self.__count - 1:
+                self.__tail = current
+        self.__count -= 1
 
     def __str__(self):
         return f"[{', '.join(str(el) for el in self)}]"
 
     def __iter__(self):
-        self.current_iterator = self.head
+        self.__current_iterator = self.__head
         return self
 
     def __next__(self):
-        while self.current_iterator is not None:
-            item = self.current_iterator.get_data()
-            self.current_iterator = self.current_iterator.get_next()
+        while self.__current_iterator is not None:
+            item = self.__current_iterator.get_data()
+            self.__current_iterator = self.__current_iterator.get_next()
             return item
         raise StopIteration
